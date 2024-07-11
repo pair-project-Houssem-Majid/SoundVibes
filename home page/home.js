0832b171c9271975601912023bc354eb7ae23bea
NciriAbdelmajid <abdelmajidnciri872@gmail.com>
2024-07-11 08:35:02 +0100

    Home page

---
+++ home page/home.js
@@ -0,0 +1,279 @@
+var SongAlbums=[
+    {
+        name:'.Damn',
+        image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+
+        ]
+    },
+    {
+        name:'Mr.Morale',
+        image:'./images/kendrick lamar new album cover.jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+            
+
+        ]
+    },
+    {
+        name:'.Damn',
+        image:'./images/To Pimp A Butterfly.jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+
+        ]
+    },
+    {
+        name:'.Damn',
+        image:'./images/télécharger (4).jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+
+        ]
+    },
+    {
+        name:'.Damn',
+        image:'./images/Black Panther The Album Music From And Inspired By - Kendrick Lamar.jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            },
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+
+        ]
+    },
+    {
+        name:'Not Like Us',
+        image:'./images/Not Like Us Kendrick Lamar.jpeg',
+        artist:"Kendrick Lamar",
+        tracks:[
+            {
+                title:'Fear',
+                artist:'Kendrick Lamar',
+                image:'./images/Kendrick Lamar - DAMN_ (2017).jpeg',
+                music:'./Songs/FEAR..mp3'
+            }
+
+        ]
+    }
+
+]
+function each(coll, f) {
+    if (Array.isArray(coll)) {
+      for (var i = 0; i < coll.length; i++) {
+        f(coll[i], i);
+      }
+    } else {
+      for (var key in coll) {
+        f(coll[key], key);
+      }
+    }
+  }
+  function map(array, func) {
+    var acc = [];
+    each(array, function(element, i) {
+      acc.push(func(element, i));
+    });
+    return acc;
+  }
+
+$(document).ready(function() {
+    return map(SongAlbums,function(album) {
+        var albumElem =$('<div class="album"></div>');
+        var albumHeader =$('<div class="album-header"></div>');
+        var albumDetail =$('<div class="album-details"></div>');
+        
+        albumHeader.append('<h2 class="album-name">'+album.name+'</h2>');
+        albumHeader.append('<img class="album-image" src="'+album.image+ '" alt="'+ album.name +'">');
+        albumDetail.append('<p>Artist: '+album.artist+'</p>');
+
+        var trackList = $('<div class="track-list" style="display: none;"></div>');
+
+        for (var i = 0; i < album.tracks.length; i++) {
+            var track = album.tracks[i];
+            var trackElement = $('<div class="track"></div>');
+            trackElement.append('<h3>'+track.title+'</h3>');
+            trackElement.append('<p>Artist: '+track.artist+'</p>');
+            trackElement.append('<img src="'+track.image+ '" alt="'+track.title+'" class="track-image">');
+            trackElement.append('<audio controls><source src="'+track.music+'" type="audio/mpeg"></audio>');
+            trackList.append(trackElement);
+        }
+
+        albumElem.append(albumHeader);
+        albumElem.append(albumDetail);
+        albumElem.append(trackList);
+        $('#albums-container').append(albumElem);
+
+        albumHeader.click(function() {
+            trackList.slideToggle();
+        });
+    });
+});
+
+// $('#albums-container').css("background-image","url(images/phone_035.jpg)")
